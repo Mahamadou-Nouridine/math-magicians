588121a19f4317e72af0005758c5c873c3c1f0e4
Mahamadou-Nouridine <98597449+Mahamadou-Nouridine@users.noreply.github.com>
2023-05-29 22:08:28 +0100

    Build the Calculator component

---
+++ src/components/calculator.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Calculator = () => (
+  <>
+    <div className="calculator">
+      <div className="result">0</div>
+      {/* symboles */}
+      <div>AC</div>
+      <div>+/-</div>
+      <div>%</div>
+      <div className="orange">÷</div>
+      {/* symboles */}
+
+      {/* Numbers */}
+      <div>7</div>
+      <div>8</div>
+      <div>9</div>
+      <div className="orange">&#215;</div>
+      <div>4</div>
+      <div>5</div>
+      <div>6</div>
+      <div className="orange">-</div>
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div className="orange">+</div>
+      {/* Numbers */}
+      <div className="zero">Zero</div>
+      <div>.</div>
+      <div className="orange">=</div>
+    </div>
+  </>
+);
+
+export default Calculator;
